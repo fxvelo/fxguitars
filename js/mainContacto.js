afc3c632af9a70855e9543ebb0c8ccc66bbcc4ba
fxvelo <103290493+fxvelo@users.noreply.github.com>
2024-12-06 17:23:28 -0300

    Update mainContacto.js

--- js/mainContacto.js
+++ js/mainContacto.js
@@ -2,27 +2,28 @@
 
 /* <main> de contacto.html */
 const mainContactoJS = `
-<div id="contacto-container">
-<h2>Contacto</h2>
+    <div id="contacto-container">
+        <h2>Contacto</h2>
 
-<form action="https://formspree.io/f/xyzyklpj" method="POST">
-    <div>               
-        <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre" required>
-    </div>
+        <form action="https://formspree.io/f/xyzyklpj" method="POST">
+        <!--  action="https://formspree.io/f/xyzyklpj" -->
+            <div>               
+                <label for="nombre">Nombre:</label>
+                <input type="text" name="nombre" required>
+            </div>
 
-    <div> 
-        <label for="email">Correo electrónico:</label>
-        <input type="text" name="email" required>
-    </div>
+            <div> 
+                <label for="email">Correo electrónico:</label>
+                <input type="text" name="email" required>
+            </div>
 
-    <div> 
-        <label for="mensaje">Mensaje:</label>
-        <textarea name="mensaje" rows=4 required></textarea>
-    </div>
+            <div> 
+                <label for="mensaje">Mensaje:</label>
+                <textarea name="mensaje" rows=4 required></textarea>
+            </div>
 
-    <button type="submit" title="Haga clic para enviar">Enviar</button>
-</form>
-</div>`;
+            <button type="submit" title="Haga clic para enviar">Enviar</button>
+        </form>
+    </div>`;
 
 document.querySelector("#mainContacto").innerHTML = mainContactoJS;
