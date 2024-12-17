d5a746851e53e6a19a487d563323c8daa2612493
fxvelo <103290493+fxvelo@users.noreply.github.com>
2024-12-17 12:03:44 -0300

    Update headerFooter.js 17/12/2024

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -7,74 +7,74 @@ function loadMain(value) {
 
 /* Header */
 const headerJS = `
-        <a id="logo" href="index.html" onclick="loadMain('index')">fx guitars</a>
+    <a id="logo" href="index.html" onclick="loadMain('index')">fx guitars</a>
 
-        <nav id="navbar">
-            <a href="index.html" onclick="loadMain('index')">Inicio</a>
-            <a href="productos.html" onclick="loadMain('productos')">Productos</a>
-            <a href="acerca.html" onclick="loadMain('acerca')">Acerca de</a>
-            <a href="contacto.html" onclick="loadMain('contacto')">Contacto</a>
-        </nav>
+    <nav id="navbar">
+        <a href="index.html" onclick="loadMain('index')">Inicio</a>
+        <a href="index.html" onclick="loadMain('productos')">Productos</a>
+        <a href="index.html" onclick="loadMain('acerca')">Acerca de</a>
+        <a href="index.html" onclick="loadMain('contacto')">Contacto</a>
+    </nav>
 
-        <div id="icons">
-            <div id="cart-btn"><i class="fa-solid fa-cart-shopping"></i></div>
-            <div><i class="fas fa-user"></i></div>
-            <div id="menu-btn"><i class="fas fa-bars"></i></div>
+    <div id="icons">
+        <div id="cart-btn"><i class="fa-solid fa-cart-shopping"></i></div>
+        <div><i class="fas fa-user"></i></div>
+        <div id="menu-btn"><i class="fas fa-bars"></i></div>
+    </div>
+    
+    <!--Cart-->
+    <div id="cart">
+        <h3 class="cart-title">Carrito</h3>
+
+        <!--Cart content-->
+        <ul class="listCart"></ul>
+        <div class="checkout">
+            <div class="total">$ 0 </div>
+            <div class="cart-close">Cerrar</div>
         </div>
-        
-        <!--Cart-->
-        <div id="cart">
-            <h3 class="cart-title">Carrito</h3>
-
-            <!--Cart content-->
-            <ul class="listCart"></ul>
-            <div class="checkout">
-                <div class="total">$ 0 </div>
-                <div class="cart-close">Cerrar</div>
-            </div>
-        </div>`;
+    </div>`;
 
 document.querySelector("header").innerHTML = headerJS;
 
 /* Footer */
 const footerJS = `
-        <div id="footer-content">
-            <div class="footer-box">
-                <a id="footer-logo" href="#">fx guitars</a>
+    <div id="footer-content">
+        <div class="footer-box">
+            <a id="footer-logo" href="#">fx guitars</a>
 
-                <div class="social">
-                    <a href="#" class="fab fa-facebook" alt="Facebook"></a>
-                    <a href="#" class="fa-brands fa-square-instagram" alt="Instagram"></a>
-                    <a href="#" class="fab fa-twitter" alt="X"></a>
-                </div>
-            </div>
-
-            <div class="footer-box">
-                <h3>Contactanos</h3>
-                <a href="#">+ 54 9 11 7777 7777</a>
-                <a href="contacto.html">fxguitars@gmail.com</a>
-                <a href="index.html#location">Dr. Desiderio Fernando Davel 600, C1414 Cdad. Autónoma de Buenos Aires</a>
-            </div>
-
-            <div class="footer-box">
-                <h3>Enlaces</h3>
-                <a href="index.html">Inicio</a>
-                <a href="productos.html">Productos</a>
-                <a href="acerca.html">Acerca de</a>
-                <a href="contacto.html">Contacto</a>
-            </div>
-
-            <div class="footer-box">
-                <h3>Novedades</h3>
-                <p>Suscribite para recibir novedades:</p>
-                <form action="#" method="post">
-                    <input type="email" name="" id="" placeholder="Email...">
-                    <input type="submit" value="Suscribirse">
-                </form>
+            <div class="social">
+                <a href="#" class="fab fa-facebook" alt="Facebook"></a>
+                <a href="#" class="fa-brands fa-square-instagram" alt="Instagram"></a>
+                <a href="#" class="fab fa-twitter" alt="X"></a>
             </div>
         </div>
 
-        <div id="copyright">&copy; 2024 · Fernando Velo · 24233</div>`;
+        <div class="footer-box">
+            <h3>Contactanos</h3>
+            <a href="#">+ 54 9 11 7777 7777</a>
+            <a href="contacto.html">fxguitars@gmail.com</a>
+            <a href="index.html#location">Dr. Desiderio Fernando Davel 600, C1414 Cdad. Autónoma de Buenos Aires</a>
+        </div>
+
+        <div class="footer-box">
+            <h3>Enlaces</h3>
+            <a href="index.html">Inicio</a>
+            <a href="productos.html">Productos</a>
+            <a href="acerca.html">Acerca de</a>
+            <a href="contacto.html">Contacto</a>
+        </div>
+
+        <div class="footer-box">
+            <h3>Novedades</h3>
+            <p>Suscribite para recibir novedades:</p>
+            <form action="#" method="post">
+                <input type="email" name="" id="" placeholder="Email...">
+                <input type="submit" value="Suscribirse">
+            </form>
+        </div>
+    </div>
+
+    <div id="copyright">&copy; 2024 · Fernando Velo · 24233</div>`;
 
 document.querySelector("footer").innerHTML = footerJS;
 
